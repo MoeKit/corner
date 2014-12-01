8bcf113703e37671c36bfde8355b14aad97123e9
airyland <airyland@qq.com>
2014-12-01 20:34:58 +0800

    update

--- examples/index.md
+++ examples/index.md
@@ -2,12 +2,74 @@
 
 ---
 
-## Normal usage
+## 颜色
 
 <div id="target" style="width:150px;height:150px;background:gray;"></div>
 
 ````javascript
-seajs.use('index', function(corner) {
-
+seajs.use('index', function(Corner) {
+	new Corner({
+	target:'#target',
+	color:'green'
+	});
 });
 ````
+
+## 距离
+
+<div id="target2" style="width:100px;height:150px;background:gray;"></div>
+
+````javascript
+seajs.use('index', function(Corner) {
+	new Corner({
+	target:'#target2',
+	distance:5
+	});
+});
+````
+
+## 直接显示
+
+<div id="target3" style="width:150px;height:100px;background:gray;"></div>
+
+````javascript
+seajs.use('index', function(Corner) {
+	new Corner({
+	target:'#target3',
+	distance:5,
+	static:true
+	});
+});
+````
+
+## 线条宽度
+
+<div id="target4" style="width:150px;height:100px;background:gray;"></div>
+
+````javascript
+seajs.use('index', function(Corner) {
+	new Corner({
+	target:'#target4',
+	distance:5,
+	static:true,
+	lineWidth:1
+	});
+});
+````
+
+## 线条长
+
+<div id="target5" style="width:150px;height:100px;background:gray;"></div>
+
+````javascript
+seajs.use('index', function(Corner) {
+	var c = new Corner({
+	target:'#target5',
+	distance:5,
+	static:true,
+	lineWidth:1,
+	width:30
+	});
+});
+````
+
--- index.js
+++ index.js
@@ -1,75 +1,135 @@
-var corner;
+var corner = function(option) {
+	return this.init(option);
+};
 var tpl = require('./corner.tpl');
 var $ = require('jquery');
 var position = require('position');
-$(tpl).appendTo('body');
+
 require('./corner.css');
 
 var cid = 0;
 
-function setOffset(x) {
-	position.pin({
-		element: '.corner-top',
-		x: 0,
-		y: 0
-	}, {
-		element: '#target',
-		x: 'left-' + x + 'px',
-		y: 'top-' + x + 'px'
-	});
 
-	position.pin({
-		element: '.corner-right',
-		x: 'right',
-		y: 'top'
-	}, {
-		element: '#target',
-		x: 'right+' + x + 'px',
-		y: 'top-' + x + 'px'
-	});
+var cornerConfig = function(x) {
+	return [
+		[{
+			element: '.corner-top',
+			x: 0,
+			y: 0
+		}, {
+			element: '#target',
+			x: 'left-' + x + 'px',
+			y: 'top-' + x + 'px'
+		}],
+		[{
+			element: '.corner-right',
+			x: 'right',
+			y: 'top'
+		}, {
+			element: '#target',
+			x: 'right+' + x + 'px',
+			y: 'top-' + x + 'px'
+		}],
+		[{
+			element: '.corner-bottom',
+			x: 'right',
+			y: 'bottom'
+		}, {
+			element: '#target',
+			x: 'right+' + x + 'px',
+			y: 'bottom+' + x + 'px'
+		}],
+		[{
+			element: '.corner-left',
+			x: 'left',
+			y: 'bottom'
+		}, {
+			element: '#target',
+			x: 'left-' + x + 'px',
+			y: 'bottom+' + x + 'px'
+		}]
+	];
 
-	position.pin({
-		element: '.corner-bottom',
-		x: 'right',
-		y: 'bottom'
-	}, {
-		element: '#target',
-		x: 'right+' + x + 'px',
-		y: 'bottom+' + x + 'px'
-	});
+};
 
-	position.pin({
-		element: '.corner-left',
-		x: 'left',
-		y: 'bottom'
-	}, {
-		element: '#target',
-		x: 'left-' + x + 'px',
-		y: 'bottom+' + x + 'px'
-	});
-}
-
-setOffset(0);
-$('#target').mouseenter(function() {
-	$('.corner').addClass('corner-show');
-	function animate(){
-		setTimeout(function(){
-			setOffset(5);
-		},0);
-		setTimeout(function(){
-			setOffset(8);
-		},500);
+corner.prototype.init = function(option) {
+	this.$target = $(option.target);
+	this.appendCorners();
+	this._setCid(++cid);
+	if (option.color) {
+		this.setColor(option.color);
 	}
-	setOffset(8);
-	setInterval(function(){
-		//animate();
-	},1000);
-}).mouseleave(function() {
-	$('.corner').removeClass('corner-show')
-	setOffset(0);
-});
+	if (option.lineWidth) {
+		this.setLineWidth(option.lineWidth);
+	}
+	if (option.width) {
+		this.setWidth(option.width);
+	}
+	this.o = option;
+	this._setPosition();
+	this.bind();
+	this.$corners.show();
+	return this;
+};
 
+// append corners 
+corner.prototype.appendCorners = function() {
+	this.$corners = $(tpl).hide().appendTo('body');
+};
 
+// 
+corner.prototype._setCid = function(cid) {
+	this.cid = cid;
+	this.$corners.addClass('mk-corner-' + cid);
+};
 
-console.log(tpl);
+// set position
+corner.prototype._setPosition = function(x) {
+	var config = $.extend([], cornerConfig(x));
+	for (var i = 0; i < config.length; i++) {
+		config[i][0].element = config[i][0].element + '.mk-corner-' + this.cid;
+		config[i][1].element = this.$target;
+		position.pin(config[i][0], config[i][1]);
+	}
+};
+
+// set color
+corner.prototype.setColor = function(color) {
+	this.$corners.css('border-color', color);
+	return this;
+};
+
+// set line width
+corner.prototype.setLineWidth = function(width) {
+	this.$corners.css('border-width', width+'px');
+	return this;
+};
+
+// set line width
+corner.prototype.setWidth = function(width) {
+	this.$corners.css('width', width+'px');
+	this.$corners.css('height', width+'px');
+	return this;
+};
+
+// bind mouse events
+corner.prototype.bind = function() {
+	var distance = this.o.distance || 10;
+	var _this = this;
+	if (this.o.static === true) {
+		_this.$corners.addClass('corner-show');
+		_this._setPosition(distance);
+	} else {
+		_this._setPosition(0);
+		this.$target.mouseenter(function() {
+			_this.$corners.addClass('corner-show');
+			_this._setPosition(distance);
+		}).mouseleave(function() {
+			_this.$corners.removeClass('corner-show')
+			_this._setPosition(0);
+		});
+	}
+
+};
+
 module.exports = corner;
